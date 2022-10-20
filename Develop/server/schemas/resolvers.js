deb6c648e356aa416a18d00bfad2c120e5739867
sonjailic <sonjailic94@hotmail.com>
2022-10-19 21:37:50 -0400

    add typeDefs and start resolvers, querys and mutations

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,17 @@
+const resolvers = {
+    Query: {
+        // me for user
+
+    }
+
+    Mutation: {
+        //login, returns an auth type
+        //addUser, accepts a username, email and password as params, returns auth type
+        //saveBook, accepts a book authos arry, description, title, bookId, image and link
+        //as params, returns a User (use "input")
+        //removeBook, accepts bookId as a param, returns User type
+
+    }
+};
+
+module.exports = resolvers;
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -2,7 +2,35 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
